66ef1a0859b481a4055d571e1c193e99b7a429f9
limoh653 <limohesbon7@gmail.com>
2024-09-03 22:23:22 +0300

    all test passing

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,37 @@
 class Song:
-    pass
+        # Class attributes for keeping track of songs, artists, genres, and counts
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+        # Increment the song count each time a new song is created
+        Song.count += 1
+
+        # Add artist to the artists list if not already present
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        # Add genre to the genres list if not already present
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        # Update genre count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Update artist count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+
+pass
